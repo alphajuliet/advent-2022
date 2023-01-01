e988c047ba603e99c0954089c663c1f90c0483b7
alphajuliet <andrew@alphajuliet.com>
2023-01-01 15:33:30 +1100

    Complete day 7 part 1

--- src/aoc22/day07.clj
+++ src/aoc22/day07.clj
@@ -13,17 +13,11 @@
        slurp))
 
 (def log-parser
-  "Parse the log file"
   (insta/parser
-   "<log> := folder+
-    folder := request response+
-
-    <request> := chdir ls
-    chdir := cd+
-    <cd> := <'$ cd '> #'[a-z./]+' <newline>
-    <ls> := <'$ ls'> <newline>
-
-    <response> := (dir | file) <newline>
+   "<log> := (line <newline>)+
+    <line> := cd | <ls> | dir | file
+    cd := <'$ cd '> name
+    ls := <'$ ls'>
     dir := <'dir '> name
     file := size <' '> name
 
@@ -31,20 +25,74 @@
     size := #'\\d+'
     newline := '\\n'"))
 
+(defn transform-log
+  [log]
+  (->> log
+       (insta/transform
+        {:size #(edn/read-string %)
+         :name identity
+         })))
+
+(defn chdir
+  [cwd dir]
+  (case dir
+    ".." (vec (butlast cwd))
+    "/" (vector dir)
+    ;; else
+    (conj cwd dir)))
+
+(defn add-dir
+  [cmd cwd fs]
+  (-> fs
+      (assoc (conj cwd (second cmd)) {:dir 0})))
+
+(defn add-file
+  [cmd cwd fs]
+  (as-> fs <>
+    (assoc <> (conj cwd (last cmd)) {:file (second cmd)})
+    (loop [fs_ <>
+           path cwd]
+      (if (empty? path)
+        fs_
+        ;; else
+        (recur (update-in fs_ [path :dir] #(+ % (second cmd)))
+               (vec (butlast path)))))))
+
+(defn create-fs
+  "Create a file system as a map of paths"
+  ;; Props to https://github.com/hunkyjimpjorps/AdventOfCode/blob/main/2022/day-07/day-07.rkt
+  [log]
+  (loop [[cmd & cmds] log
+         cwd ["/"]
+         fs {["/"] {:dir 0}}]
+    (case (first cmd)
+      nil fs
+      :cd (recur cmds (chdir cwd (second cmd)) fs)
+      :dir (recur cmds cwd (add-dir cmd cwd fs))
+      :file (recur cmds cwd (add-file cmd cwd fs))
+      ;; else
+      (assert false)
+      )))
+
 (defn read-log
   "Parse the log file and convert strings to numbers"
   [log]
   (->> log
        log-parser
-       (insta/transform
-        {:size #(vector :size (edn/read-string %))})))
+       transform-log))
 
 ;;------------------------------
 (defn part1
   [f]
   (->> f
        read-data
-       read-log))
+       read-log
+       create-fs
+       vals
+       (filter :dir)
+       (map (comp first vals))
+       (filter #(<= % 100000))
+       (apply +)))
 
 ;; (assert (= 0 (part1 testf)))
 
