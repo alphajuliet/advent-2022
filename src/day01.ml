24c343c385c46bee4ed5bccf5c9e5fc9222391fc
alphajuliet <andrew@alphajuliet.com>
2023-06-17 16:00:09 +1000

    Refactor and tidy day01.ml

--- src/day01.ml
+++ src/day01.ml
@@ -10,20 +10,9 @@ let inputf = "../data/day01-input.txt"
 (** Read in all the lines from a file *)
 let read_lines file =
   let contents = In_channel.with_open_bin file In_channel.input_all in
-  (* String.split_on_char '\n' contents *)
   contents
   |> Str.split (Str.regexp "\n\n")
-  |> List.map ~f:(String.split_on_char '\n')
-
-(** Split and group an array separated on empty strings *)
-let split_by_empty_string lst =
-  let add_to_group acc x =
-    if x = "" then [] :: acc
-    else match acc with
-         | [] -> [[x]]
-         | (h :: t) -> (x :: h) :: t
-  in
-  List.fold_left add_to_group [[]] lst |> List.rev |> List.filter (fun x -> x <> [])
+  |> List.map (Str.split (Str.regexp "\n"))
 
 (** Convert a nested list of strings to integers *)
 let nested_string_to_int_list = List.map (fun x -> List.map int_of_string x)
@@ -37,14 +26,12 @@ let rec take k xs =
 
 let part1 file =
   read_lines file
-  |> split_by_empty_string
   |> nested_string_to_int_list
   |> List.map sum_list
   |> max_number_list
 
 let part2 file =
   read_lines file
-  |> split_by_empty_string
   |> nested_string_to_int_list
   |> List.map sum_list
   |> List.sort compare
